6739ff1985561bb4d58e6604ed4e7ea8e0657784
Doszhan <doszhan@mail.com>
2021-03-11 13:57:21 +0600

    add translate en-ru

--- Newspaper/news/apps.py
+++ Newspaper/news/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class NewsConfig(AppConfig):
     name = 'news'
+
+# нам надо переопределить метод ready, чтобы при готовности нашего приложения импортировался модуль
+# со всеми функциями обработчиками
+    def ready(self):
+        import news.signals
